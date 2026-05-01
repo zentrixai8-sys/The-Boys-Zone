2f9f0255f2849f2f42e102bd47606c44d09a72c4
Piramalinfo012 <info@piramalpetrolium.com>
2026-05-01 11:38:34 +0530

    mobile app

--- src/components/CategoryBar.tsx
+++ src/components/CategoryBar.tsx
@@ -63,7 +63,7 @@ export const CategoryBar = () => {
         {/* Categories Container */}
         <div 
           ref={scrollRef}
-          className="flex items-start justify-center gap-4 sm:gap-12 py-4 overflow-x-auto scrollbar-hide snap-x snap-mandatory"
+          className="flex items-start md:justify-center justify-start gap-5 sm:gap-12 py-4 px-6 overflow-x-auto scrollbar-hide snap-x snap-mandatory"
         >
           {/* "All" Category */}
           <motion.div
