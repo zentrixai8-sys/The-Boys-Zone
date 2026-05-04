55e2b7c03c722577117e0b9d491af47f8187722a
Piramalinfo012 <info@piramalpetrolium.com>
2026-05-04 22:59:28 +0530

    payment Funtion

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -484,7 +484,7 @@ export const ProductDetail = () => {
       </div>
 
       {/* Mobile Sticky Bottom Bar */}
-      <div className="md:hidden fixed bottom-0 left-0 right-0 z-50 bg-white border-t border-gray-100 p-3 flex gap-3 shadow-[0_-10px_30px_rgba(0,0,0,0.05)]">
+      <div className="md:hidden fixed bottom-[56px] left-0 right-0 z-50 bg-white border-t border-gray-100 p-3 flex gap-3 shadow-[0_-10px_30px_rgba(0,0,0,0.05)]">
         <button 
           onClick={() => {
             if (currentStock <= 0) {
